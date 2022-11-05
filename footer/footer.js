896df93bf9076eccd06f527f2719313be8672c9f
nydomi <nydomi1@gmail.com>
2022-11-05 15:36:18 +0100

    Elrendezes

--- footer/footer.js
+++ footer/footer.js
@@ -2,7 +2,7 @@ fetch('footer/footer.html')
 .then(res => res.text())
 .then(text => {
     let oldelem = document.querySelector("script#replace_with_footerbar");
-    let newelem = document.createElement("div");
+    let newelem = document.createElement("footer");
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
 })
